52e11b9468673b6993b5b83714500191c9268efd
geonnyeong oh <geonnyeongoh@geonnyeongs-MacBook-Air.local>
2023-10-10 16:05:07 +0900

    second

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs')
 app.set('vews', './views')
 
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(express.static(__dirname + '/public')) //이런식으로 넣어준다. 이미지
 
 //라우팅
 app.get('/', (req, res) => {
@@ -46,14 +46,35 @@ app.post('/contactProc', (req, res) =>{
     connection.query(sql, function(err, result){
         if(err) throw err
         console.log('자료 1개를 삽입했습니다.')
-        res.send("<script> alert('문의사항이 등록되었습니다.'); location.href='/';</script>")
+        res.send("<script> alert('문의사항이 등록되었습니다.'); location.href='/';</script>"); 
 
-    })
-    var a = `${name} ${phone} ${email} ${memo}`
+    }) //왜 알람안뜰까나?
+    // var a = `${name} ${phone} ${email} ${memo}`
 
-    res.send(a);
+    // res.send(a);
 })
 
+app.get('/contactDelete', (req, res)=>{
+    var idx = req.query.idx
+    var sql = `delete from contact where idx='${idx}' `
+
+    connection.query(sql, function(err, result){
+        if(err) throw err
+        res.send("<script> alert('삭제되었습니다.'); location.href='/contactlist';</script>")
+        
+
+    })
+})
+
+
+app.get('/contactlist', (req, res)=>{
+    var sql = `select * from contact order by idx desc`
+    connection.query(sql, function(err, results, fields){
+        if (err) throw err;
+        console.log(results);
+        res.render('contactlist', {lists:results})
+    })
+})
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
